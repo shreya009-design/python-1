d1923ad987c00d186bd2d03eca031c2a9b9ad0bd
shreya009-design <1977shriop@gmail.com>
2026-01-21 23:29:23 +0530

    Add circle area calculation in 19.py

---
+++ 19.py
@@ -0,0 +1,8 @@
+// 19. Circle: A = 22/7 * R * R
+double radius, area_cir;
+printf("Enter radius of circle: ");
+scanf("%lf", &radius);
+
+area_cir = (22.0 / 7.0) * radius * radius;
+
+printf("Circle -> Area = %.2f\n", area_cir);
