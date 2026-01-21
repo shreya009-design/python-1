4db5d3d19ba7de19cd62122475ddc9f3163eefd7
shreya009-design <1977shriop@gmail.com>
2026-01-21 23:33:07 +0530

    Convert rectangle calculations from C to Python

--- 18.py
+++ 18.py
@@ -1,9 +1,8 @@
-// 18. Rectangle: A = L * B, P = 2(L + B)
-double length, breadth, area_rec, peri_rec;
-printf("Enter length and breadth of rectangle: ");
-scanf("%lf %lf", &length, &breadth);
+# 18. Rectangle: A = L * B, P = 2(L + B)
+length = float(input("Enter length of rectangle: "))
+breadth = float(input("Enter breadth of rectangle: "))
 
-area_rec = length * breadth;
-peri_rec = 2 * (length + breadth);
+area_rec = length * breadth
+peri_rec = 2 * (length + breadth)
 
-printf("Rectangle -> Area = %.2f, Perimeter = %.2f\n", area_rec, peri_rec);
+print("Rectangle -> Area =", area_rec, "Perimeter =", peri_rec)
