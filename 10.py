cad118c4610781da0b703e2080c1cfffbbca8c03
shreya009-design <1977shriop@gmail.com>
2026-01-21 23:22:31 +0530

    Add currency conversion from dollars to pounds

---
+++ 10.py
@@ -0,0 +1,5 @@
+# 10. Convert dollars into pound (1 $ = 48 Rs, 1 pound = 70 Rs.)
+dollars = float(input("Enter amount in dollars: "))
+rupees = dollars * 48
+pounds = rupees / 70
+print("Pounds =", pounds)
