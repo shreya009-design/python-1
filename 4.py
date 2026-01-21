90ed52667a82268d16dd29d7912c8e89709bdd3f
shreya009-design <1977shriop@gmail.com>
2026-01-21 23:19:10 +0530

    Add division functionality with zero check

---
+++ 4.py
@@ -0,0 +1,7 @@
+# 4. Divide two numbers
+a = int(input("\nEnter dividend: "))
+b = int(input("Enter divisor: "))
+if b != 0:
+    print("Quotient =", a / b)
+else:
+    print("Cannot divide by zero")
